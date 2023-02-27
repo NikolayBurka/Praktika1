cf6800f1ec6173c8eba076bd2791fd2968901e21
Nikolay Burka <nikolay.burka@bk.ru>
2023-02-27 09:07:29 +0200

    add z1
    Задача на вывод двузначных чисел в столбик

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿for (int i = 10; i <= 99; i++)
+    if (i % 10 + i / 10 == (i * 2) % 10 + (i * 2) / 10 % 10 + (i * 2) / 100)
+    {
+        Console.Write("{0}\t", i);
+    }
+int count = 0;
+for (int x = 2; x <= 99; x++)
+{
+    if (x % 2 == 0)
+    {
+        Console.WriteLine(x); count++;
+    }
+}
+Console.WriteLine($"кол-во={count}");
